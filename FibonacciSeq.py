17c53118e8ba9287d69236fe6495dc81fdec586d
Michael Obed <mikeoduoli@gmail.com>
2023-07-01 22:42:16 +0300

    The Second, Checks Fibonacci.

---
+++ FibonacciSeq.py
@@ -0,0 +1,10 @@
+def checkFibonacci(number):
+    if number < 0:
+        return False
+
+    a, b = 0, 1
+    while b < number:
+        a, b = b, a + b
+
+    return b == number
+
---
+++ Index.py
@@ -0,0 +1,13 @@
+def primeNum(number):
+    if number <= 1:
+        return False
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
+
+
+primeNum(19)
+primeNum(24)
